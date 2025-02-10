638e08c36e43982ce62b81fecf03a8aac1b5bb9b
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-02-09 19:53:27 -0500

    Creando mi primer paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Miguel Santiago Gómez Suárez",                         # Tu nombre
+    author_email="hitomiblood@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Hitomiblood/tercerRepo",     # URL del proyecto
+)
